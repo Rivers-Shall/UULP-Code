67f68d30e9c95dd2bae5e78b1346ccae9997203a
Rivers Xiao <rivers-shall@qq.com>
2019-03-22 19:20:35 +0800

    Read from /dev/tty and set terminal mode
    terminal mode: noncanonical mode, no character echo

--- Chap01/src/more.c
+++ Chap01/src/more.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <termios.h>
 
 int num_of_lines = 0;
+struct termios old;
+FILE *cmd;
+
 const int LEN_OF_LINE = 512;
+const char *CMD_FILE = "/dev/tty";
 
 void
 init_more();
+
+void
+exit_more();
 
 void
 do_more(FILE *);
@@ -35,10 +43,26 @@ main(int argc, char *argv[], char *env[]) {
 }
 
 /*
+ * set the terminal back to normal mode
+ */
+void
+exit_more() {
+  tcsetattr(STDIN_FILENO, TCSANOW, &old);
+  fclose(cmd);
+}
+
+/*
+ * call exit_more at exit
  * get number of lines of the terminal
+ * set the terminal to not echo keypress and send it immediately
+ * open /dev/tty as the input of commands
  */
 void
 init_more() {
+  // call exit_more at exit
+  atexit(exit_more);
+
+  // get number of lines
   char * lines_str = getenv("LINES");
   struct winsize ws;
   if (lines_str != NULL) {
@@ -48,7 +72,18 @@ init_more() {
   } else {
     num_of_lines = 24;
   }
-  //printf("num_of_lines: %d\n", num_of_lines);
+
+  // set the terminal mode
+  struct termios tm;
+  tcgetattr(STDIN_FILENO, &old);
+  tm = old;
+  tm.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &tm);
+
+  // open /dev/tty
+  if ((cmd = fopen(CMD_FILE, "r")) == NULL) {
+    exit(1);
+  }
 }
 
 void
@@ -74,7 +109,7 @@ int
 see_more() {
   char c;
   fputs("\033[7m more? \033[m", stdout);
-  while ((c = getchar()) != EOF) {
+  while ((c = fgetc(cmd)) != EOF) {
     switch (c) {
       case ' ':
         return num_of_lines;
